66dc0591608a06c90dc99ef4ed8976b997df1717
Vlad Monachenkov <cssvam@yandex.ru>
2022-03-28 18:51:51 +0300

    remove 404 favicon error

--- src/main/java/com/devel/pricetracker/application/controllers/web/IndexWebController.java
+++ src/main/java/com/devel/pricetracker/application/controllers/web/IndexWebController.java
@@ -17,10 +17,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -165,6 +162,12 @@ public class IndexWebController {
     public String parseAllAction() {
         priceParser.parseAll();
         return "redirect:/";
+    }
+
+    @GetMapping("/favicon.ico")
+    @ResponseBody
+    public void returnNoFavicon() {
+
     }
 
     private void cleanPriceDuplicates(ItemEntity itemEntity) {
